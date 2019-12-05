54a5f0046ac1935c3c06db0cd4e06066dfb5edc2
rfcbeto <rfcbeto@gmail.com>
2019-12-04 22:24:28 -0300

    inclusão do projeto.

---
+++ src/main/java/br/com/roberto/InicializaSpringboot.java
@@ -0,0 +1,12 @@
+package br.com.roberto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicializaSpringboot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InicializaSpringboot.class, args);
+	}
+}
